06c3341724196ace406e96184f82bef1d9cfd1cd
Ray <leigeosxdi@163.com>
2022-10-21 14:59:10 +0800

    init

---
+++ src/main/java/com/rs/demo/SCGatewayApp.java
@@ -0,0 +1,19 @@
+package com.rs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述
+ *
+ * @auhtor sunlei
+ * @since 2022/10/21 11:55
+ */
+@SpringBootApplication
+public class SCGatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SCGatewayApp.class, args);
+    }
+
+}
